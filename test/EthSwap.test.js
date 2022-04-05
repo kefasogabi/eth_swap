c16a69f08a3a5ec76eb5ba7b3cf1565e600fb968
Kefas Ogabi <kefasogabi@gmail.com>
2022-04-05 23:06:01 +0100

    Unknown record

---
+++ test/EthSwap.test.js
@@ -0,0 +1,39 @@
+const Token = artifacts.require('Token');
+const EthSwap = artifacts.require('EthSwap');
+
+require('chai').use(require('chai-as-promised')).should()
+
+function tokens(n){
+    return web3.utils.toWei(n, 'ether')
+}
+
+contract('EthSwap', (account) => {
+
+    let token;
+    let ethSwap;
+    before(async () => {
+        token = await Token.new()
+        ethSwap = await EthSwap.new(token.address)
+        await token.transfer(ethSwap.address, tokens('1000000'))
+    })
+
+    describe('Token deployment', async () => {
+        it('contract has a name', async () => {
+            const name = await token.name()
+            assert.equal(name, 'DApp Token')
+        })
+    })
+
+    describe('EthSwap deployment', async () => {
+        it('contract has a name', async () => {
+            const name = await ethSwap.name()
+            assert.equal(name, 'EthSwap Instant Exchange')
+        })
+
+        it('Contract has token', async () => {
+            let balance = await token.balanceOf(ethSwap.address)
+            assert.equal(balance.toString(), tokens('1000000'))
+        })
+    })
+
+})
