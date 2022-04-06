0bff9e205ed5d91957b93d4e7621a885d482603e
Kefas Ogabi <kefasogabi@gmail.com>
2022-04-06 07:44:26 +0100

    First

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -5,10 +5,22 @@ import "./Token.sol";
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
+    uint public rate = 100;
+
+    event TokenPurchased(address account, address token, uint amount, uint rate);
 
     constructor(Token _token) public {
         token = _token;
     }
 
+    function buyTokens() public payable {
+        uint tokenAmount = msg.value * rate;
+        
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        token.transfer(msg.sender, tokenAmount);
+
+        emit TokenPurchased((msg.sender), address(token), tokenAmount, rate);
+    }
 
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const Token = artifacts.require('Token');
 const EthSwap = artifacts.require('EthSwap');
 
@@ -7,7 +9,7 @@ function tokens(n){
     return web3.utils.toWei(n, 'ether')
 }
 
-contract('EthSwap', (account) => {
+contract('EthSwap', ([deployer, investor]) => {
 
     let token;
     let ethSwap;
@@ -36,4 +38,26 @@ contract('EthSwap', (account) => {
         })
     })
 
+    describe('buyTokens()', async () => {
+        let results
+        before(async () => {
+            results = await  ethSwap.buyTokens({from: investor, value: web3.utils.toWei('1', 'ether')})
+        })
+        it('Allows users to instantly purchase tokens from ethSwap for a fixed price', async () => {
+            let investorBalance = await token.balanceOf(investor)
+            assert.equal(investorBalance.toString(), tokens('100'))
+
+            let ethSwapBalance
+            ethSwapBalance = await token.balanceOf(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), tokens('999900'))
+            ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), web3.utils.toWei('1', 'Ether'))
+
+            const event = results.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount.toString(), tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
+        })
+    })
 })
